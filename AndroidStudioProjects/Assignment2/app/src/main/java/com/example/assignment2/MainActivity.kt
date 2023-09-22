a09f928569d7a24fd02543e8aab99a056796e406
Thanh <thanhthuynh7@gmail.com>
2023-09-22 17:09:48 -0400

    restructured git repo and implemented SnackBar Challenge

---
+++ AndroidStudioProjects/Assignment2/app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.assignment2
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Snackbar.make(view, R.string.correct_toast, Snackbar.LENGTH_SHORT).show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Snackbar.make(view, R.string.incorrect_toast, Snackbar.LENGTH_SHORT).show()
+        }
+    }
+}
